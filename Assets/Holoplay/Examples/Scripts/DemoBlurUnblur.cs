9b9d105b5afce181112691bf08ad4891482004c6
BJKim <xcxcmath@icloud.com>
2019-11-20 13:17:38 +0900

    Initial commit

---
+++ Assets/Holoplay/Examples/Scripts/DemoBlurUnblur.cs
@@ -0,0 +1,27 @@
+﻿//Copyright 2017-2019 Looking Glass Factory Inc.
+//All rights reserved.
+//Unauthorized copying or distribution of this file, and the source code contained herein, is strictly prohibited.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace LookingGlass.Demos {
+    public class DemoBlurUnblur : MonoBehaviour {
+        public PostProcessVolume postProcessingVolume;
+        public UnityEngine.UI.Text label;
+        public bool enablePostProcessing;
+
+        void Update() {
+            if (enablePostProcessing) {
+                label.text = "Post Processing";
+                postProcessingVolume.enabled = true;
+            } else {
+                label.text = "No Post Processing";
+                postProcessingVolume.enabled = false;
+            }
+        }
+    }
+}
